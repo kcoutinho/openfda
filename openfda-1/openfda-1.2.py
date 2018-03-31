93c4f870022b90d5c8dc35bf5a51a4f0b5d9283b
k.coutinho.2017 <kattycg96@gmail.com>
2018-03-31 15:44:08 +0200

    Latest version of the second part of openfda-1

--- openfda-1/openfda-1.2.py
+++ openfda-1/openfda-1.2.py
@@ -10,6 +10,7 @@ print(r1.status, r1.reason)
 repos_raw = r1.read().decode("utf-8")
 conn.close()
 repo = json.loads(repos_raw)
+
 repo = repo['results']
 for i in range(0,10):
     print("The id of the drug",i+1,"is:",repo[i]['id'])
