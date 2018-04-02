26adb301ef801c32e6115d4acbc90e005f79c19f
k.coutinho.2017 <kattycg96@gmail.com>
2018-04-02 15:55:29 +0200

    Last version

--- openfda-2/openfda-2.py
+++ openfda-2/openfda-2.py
@@ -4,13 +4,15 @@ import json
 headers = {'id': 'http-client'}
 
 conn = http.client.HTTPSConnection("api.fda.gov")
-conn.request("GET", '/drug/label.json?search=active_ingredient.0:"Salicylic acid"&limit=20', None, headers)
+conn.request("GET", '/drug/label.json?limit=20', None, headers)
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
 repos_raw = r1.read().decode("utf-8")
 conn.close()
 repo = json.loads(repos_raw)
 repo = repo['results']
+
 for i in range(0,10):
-    if "Salicylic acid" in repo[i]['active_ingredient'][0]:
+    active_ingredient = [repo[i]['active_ingredient']]
+    if "Salicylic acid" in active_ingredient:
         print("The manufactur name of the drug",i+1,"is:",repo[i]['openfda']['manufacturer_name'])
