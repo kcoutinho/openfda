cabb096d41106b1f775928128889c4fd1058e251
k.coutinho.2017 <kattycg96@gmail.com>
2018-04-03 16:39:05 +0200

    Openfda-3

--- openfda-3/openfda-3.py
+++ openfda-3/openfda-3.py
@@ -13,13 +13,13 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         # Send headers
         self.send_header('Content-type','text/html')
         self.end_headers()
-    with open ("search.html") as file_search:
+        with open ("search.html") as file_search:
 
         # Send message back to client
-        message = file_search.read()
+            message = file_search.read()
         # Write content as utf-8 data
-        self.wfile.write(bytes(message, "utf8"))
-        return
+            self.wfile.write(bytes(message, "utf8"))
+            return
 
 Handler = http.server.SimpleHTTPRequestHandler
 Handler = testHTTPRequestHandler
