0e89a21216c4b2f5f97177cd5134dc2ef7992681
k.coutinho.2017 <kattycg96@gmail.com>
2018-04-05 21:31:16 +0200

    Latest version openfda-3

--- openfda-3/openfda-3.py
+++ openfda-3/openfda-3.py
@@ -3,7 +3,7 @@ import socketserver
 import http.client
 import json
 IP="localhost"
-PORT = 8093
+PORT = 8092
 
 # HTTPRequestHandler class
 class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
@@ -22,16 +22,15 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         conn = http.client.HTTPSConnection("api.fda.gov")
         conn.request("GET", "/drug/label.json?limit=10", None, headers)
         r1 = conn.getresponse()
-        print(r1.status, r1.reason)
         repos_raw = r1.read().decode("utf-8")
         conn.close()
         repo = json.loads(repos_raw)
 
         repo = repo['results']
-        for i in range(0, 10):
-            print("The id of the drug", i + 1, "is:", repo[i]['id'])
+        for i in range(0,10):
+            message= ("The id of the drug", i + 1, "is:", repo[i]['id'])
         # Write content as utf-8 data
-        self.wfile.write(bytes(message, "utf8"))
+            self.wfile.write(bytes(message, "utf8"))
         return
 
 #Handler = http.server.SimpleHTTPRequestHandler
@@ -39,6 +38,10 @@ Handler = testHTTPRequestHandler
 
 httpd = socketserver.TCPServer(("", PORT), Handler)
 print("serving at port", PORT)
-httpd.serve_forever()
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    pass
+httpd.server_close()
 
 # https://github.com/joshmaker/simple-python-webserver/blob/master/server.py
