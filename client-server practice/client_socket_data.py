d1bfca792d47389cc08823497f8fd1e9d65ab700
k.coutinho.2017 <kattycg96@gmail.com>
2018-04-06 08:24:05 +0200

    Latest version

--- client-server practice/client_socket_data.py
+++ client-server practice/client_socket_data.py
@@ -8,11 +8,11 @@ s.connect(("www.gsyc.es", 80))
 print(s)
 # And now let's connect to our own server
 try:
-    s.connect(("localhost", 8091))
+    s.connect(("localhost", 8093))
 except OSError:
     print("Socket already used")
     # But first we need to disconnect
     s.close()
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("localhost", 8091))
+s.connect(("localhost", 8093))
 print("Read from the server", s.recv(2048).decode("utf-8"))
