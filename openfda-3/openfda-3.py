442fc3600d6a9e51ab7c8cb282165f5c80dd9b5c
k.coutinho.2017 <kattycg96@gmail.com>
2018-04-20 16:39:44 +0200

    latest version

--- openfda-3/openfda-3.py
+++ openfda-3/openfda-3.py
@@ -17,28 +17,29 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-        def drugs_names():
-            headers = {'id': 'http-client'}
-            conn = http.client.HTTPSConnection("api.fda.gov")
-            conn.request("GET", "/drug/label.json?limit=10", None, headers)
-            r1 = conn.getresponse()
-            repos_raw = r1.read().decode("utf-8")
-            conn.close()
-            repo = json.loads(repos_raw)
-            repo = repo["results"]
-            drugs = []
-            with open("drugs_html", "w") as f:
-                for i in range(0, 10):
-                    drugs_names = drugs.append.repo[i]['id']
-                    f.write(bytes(drugs, "utf8"))
 
-            with open("drugs_html","r") as f:
-                file_n = f.read()
+        headers = {'id': 'http-client'}
+        conn = http.client.HTTPSConnection("api.fda.gov")
+        conn.request("GET", "/drug/label.json?limit=10", None, headers)
+        r1 = conn.getresponse()
+        repos_raw = r1.read().decode("utf-8")
+        conn.close()
+        repo = json.loads(repos_raw)
 
-            web_contents = file_n
-            web_headers = "HTTP/1.1 200"
-            web_headers += "\n" + "Content-Type: text/html"
-            web_headers += "\n" + "Content-Length: %i" % len(str.encode(web_contents))
+        repo = repo["results"]
+        drugs = []
+        with open("drugs_html", "w") as f:
+            for i in range(0, 10):
+                drugs_names = drugs.append.repo[i]['id']
+                f.write(bytes(drugs, "utf8"))
+
+        with open("drugs_html","r") as f:
+            file_n = f.read()
+
+        web_contents = file_n
+        web_headers = "HTTP/1.1 200"
+        web_headers += "\n" + "Content-Type: text/html"
+        web_headers += "\n" + "Content-Length: %i" % len(str.encode(web_contents))
 
 
 
